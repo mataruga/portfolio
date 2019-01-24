2bda64167bd1af1216b1dc5aed9f21c58f13b2db
mataruga <danimataruga@gmail.com>
2019-01-24 18:22:44 +0100

    added click filter

--- scripts/effects.js
+++ scripts/effects.js
@@ -10,3 +10,16 @@ function shrink(element){
 function returnsize(element){
     $(element).css({"-webkit-font-smoothing":"subpixel-antialiased","transform":"perspective(1px) translateZ(0)","-webkit-transition-duration":".1s","-webkit-transform":"scale(1)","transform":"scale(1)","opacity":"1"});
 }
+
+function greenlight(element){
+    $(element).css({"background":"#53DD6C"});
+}
+function languagecol(element){
+    $(element).css({"background":"#383D3B"});
+}
+function rolecol(element){
+    $(element).css({"background":"#5C605E"});
+}
+function softwarecol(element){
+    $(element).css({"background":"#4A4E4C"});
+}
